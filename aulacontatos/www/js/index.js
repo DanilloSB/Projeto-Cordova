6ae6d243ef27f4428db53b63a378353f476965c0
dan <https://github.com/DanilloSB/Projeto-Cordova2.git>
2021-04-19 20:40:45 -0300

    adicionei a aulacontatos

---
+++ aulacontatos/www/js/index.js
@@ -0,0 +1,15 @@
+let meuapp = { 
+
+    inicializar: function(){
+        console.log("inicializar: function(){...")
+        document.addEventListener('deviceready', onDeviceReady, false);
+    }
+}
+
+
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+}
